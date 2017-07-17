17e4c3c354d234f9f3008a050d6b757c7786d2d1
lukewiwa <luke.wiwa@gmail.com>
2017-07-17 20:13:24 +1000

    wrapping in a classes

--- vault_triple_front/predict_plot.py
+++ vault_triple_front/predict_plot.py
@@ -2,45 +2,64 @@ import numpy as np
 import toyplot
 import toyplot.html
 from scipy.optimize import curve_fit
+import json
 
-x_axis = np.linspace(0, 3)
+class Linear:
+    x_axis = np.linspace(0, 3)
+
+    def __init__(self, x_values, y_values):
+        self.x, self.y = x_values, y_values
+        self.m, self.b = np.polyfit(x_values, y_values, 1)
+        self.equation = self.m * self.x_axis + self.b
+
+class Exponential():
+    x_axis = np.linspace(0, 3)
+
+    def func(self, x, a, b, c):
+        return a*np.exp(-b*x) + c
+
+    def __init__(self, x_values, y_values):
+        self.x, self.y = x_values, y_values
+        popt, pcov = curve_fit(self.func, x_values, y_values)
+        self.a, self.b, self.c = popt
+        self.equation = self.func(self.x_axis, self.a, self.b, self.c)
+    
+
+class Plot():
+    x_axis = np.linspace(0, 3)
+
+    def __init__(self, x_values, y_values):
+        self.x, self.y = x_values, y_values
+        self.linear = Linear(x_values, y_values)
+        self.exponential = Exponential(x_values, y_values)
+
+    def plot(self):
+        canvas = toyplot.Canvas(width=400, height=400)
+        axes = canvas.cartesian(
+            label="Vault Extrapolations",
+            xlabel="Rotations",
+            ylabel="Value",
+        )
+        mark1 = axes.scatterplot(
+            self.x,
+            self.y,
+            color="red",
+            size=8,
+        )
+        mark2 = axes.plot(self.x_axis, self.linear.equation)
+        mark3 = axes.plot(self.x_axis, self.exponential.equation)
+        canvas.legend([
+            ("Value", mark1),
+            ("Linear Fit", mark2),
+            ("Exponential Fit", mark3),
+            ],
+            corner=("top-left", 60, 80, 80),
+            )
+
+        toyplot.html.render(
+            canvas,
+            "vault_plot.html",
+            )
+
 salto = np.array([0, 1, 2])
 hspring = np.array([2.0, 2.8, 5.6])
-
-linear_m, linear_b = np.polyfit(salto, hspring, 1)
-linear_fit = linear_m * x_axis + linear_b
-
-def func(x, a, b ,c):
-    return a*np.exp(-b*x) + c
-
-popt, pcov = curve_fit(func, salto, hspring)
-a, b, c = popt
-exp_fit = func(x_axis, a, b, c)
-
-
-canvas = toyplot.Canvas(width=400, height=400)
-axes = canvas.cartesian(
-    label="Vault Extrapolations",
-    xlabel="Rotations",
-    ylabel="Value",
-)
-mark1 = axes.scatterplot(
-    salto,
-    hspring,
-    color="red",
-    size=8,
-)
-mark2 = axes.plot(x_axis, linear_fit)
-mark3 = axes.plot(x_axis, exp_fit)
-canvas.legend([
-    ("Value", mark1),
-    ("Linear Fit", mark2),
-    ("Exponential Fit", mark3),
-    ],
-    corner=("top-left", 60, 80, 80),
-    )
-
-toyplot.html.render(
-    canvas,
-    "vault_plot.html",
-    )
